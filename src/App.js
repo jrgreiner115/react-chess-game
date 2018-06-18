2c74956adddbad02c03c586e8518e36f6393025e
github username <jrgreiner115@gmail.com>
2018-06-18 11:36:30 -0400

    app done-ish

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state: {
+    rooms: []
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <p>login/out</p>
+        </div>
+        <h1>U PLAY CHESS?????</h1>
+        <div className='room'>
+          <Room />
+          <>Here's the room.</h3>
+        </div>
       </div>
     );
   }
