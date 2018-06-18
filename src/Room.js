e9d0d172a1967f31bcefe9745b581478166265a7
github username <jrgreiner115@gmail.com>
2018-06-18 14:47:53 -0400

    active player logic

--- src/Room.js
+++ src/Room.js
@@ -3,13 +3,28 @@ import Opponent from './Opponent'
 import User from './User'
 
 class Room extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+      // THIS BOOLEAN IS CONFUSING BUT EASIER THAN A TEXT MATCH, MAYBE? WHITE IS TRUE, BLACK IS FALSE. GAME STARTS W WHITE.
+      activePlayer: true
+    }
+  }
+
+
+  turnOperator = () => {
+    this.setState({
+      activePlayer: !this.state.activePlayer
+    })
+  }
 
   render() {
     return  (
       <div>
         <Opponent />
         <h3>Place Board here.</h3>
-        <User />
+        <User checkTurnOperator={this.turnOperator} turn={this.state.activePlayer}/>
       </div>
   )}
 }
--- src/User.js
+++ src/User.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
 class User extends Component {
-  render(){
-    return(
-      "Here's You."
-    )
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activePlayer: this.props.turn
+    }
   }
 
+
+
+  render() {
+    console.log("state is", this.state);
+    return (
+      <div>
+        {this.state.activePlayer ? <p>It's Your Turn.</p>: <p>Opponent's Turn.</p>}
+      </div>
+    )
+  }
 }
 
 export default User
